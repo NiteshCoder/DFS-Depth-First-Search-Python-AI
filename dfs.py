0180657e23e1c6b0ce1dac0bed9bd33826dd301a
Niteshkumar Prajapati <73467521+NiteshCoder@users.noreply.github.com>
2020-12-27 08:47:48 +0530

    Update dfs.py

--- dfs.py
+++ dfs.py
@@ -25,38 +25,3 @@ def dfs(visited,graph,node):
 dfs(visited,graph,'A')
 
 			
-'''
-#Depth First Search 
-
-graph = {
-	'A' : ['B','C','D'],
-	'B' : ['E'],
-	'C' : ['F','G'],
-	'D' : ['H'],
-	'E' : ['I'],
-	'F' : ['J'],
-	'I' : [],
-	'J' : [],
-	'G' : [],
-	'H' : [],
-}
-
-visited = []
-
-def dfs(visited,graph,node):
-	if 'J' not in visited:
-		print(node,end=" ")
-		if node not in visited:
-			visited.append(node)
-			for neighbours in graph[node]:
-				dfs(visited,graph,neighbours)
-	else:
-		pass
-		
-			
-dfs(visited,graph,'A')
-
-			
-	
-'''
-	
